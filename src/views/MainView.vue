5002400d80685ed7f825511fcaf4dfc2ce54d4b7
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-08-02 01:08:28 +0500

    main view

---
+++ src/views/MainView.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="main-view-wrapper main-view">
+        <heading-template>
+            <template #left-side>
+                <div class="heading-logo">
+                    <img src="../assets/vue.svg" alt="">
+                    <h2>Contacts</h2>
+                </div>
+            </template>
+            <template #right-side>
+                <n-button
+                    type="primary"
+                    @click="$router.push('/contact-create')"
+                >  
+                    <template #icon>
+                        <n-icon
+                            :component="AddOutline"
+                        />
+                    </template>
+                    Create contact
+                </n-button>
+            </template>
+        </heading-template>
+        <div class="main-view-body body">
+            <search-bar
+                class="body-search-bar"
+                @input-querry="searchDataInDb"
+                @select-update="searchDataInDb"
+            />
+            <n-scrollbar style="height: calc(100% - 75px)">
+                <contact-list
+                    v-if="!searchMode"
+                    :contact-list="sortedData"
+                />
+                <search-result-list
+                    v-else
+                    :result-list="searchData"
+                />
+                <n-empty
+                    v-if="searchResultEmpty"
+                    description="No results ☹️"
+                />
+            </n-scrollbar>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+// imports
+import { getSortedData, searchInDbByKeyWords } from '../services/dbRequests/'
+import { onBeforeMount, ref, computed } from 'vue'
+
+import headingTemplate from '../components/templates/headingTemplate.vue'
+import searchBar from '../components/searchBar.vue'
+import contactList from '../components/contactItems/contactList.vue'
+import searchResultList from '../components/contactItems/searchResultList.vue'
+import { NButton, NIcon, NScrollbar, NEmpty } from 'naive-ui'
+import { AddOutline } from '@vicons/ionicons5'
+/////////////////
+
+// data 
+const sortedData = ref([]);
+const searchData = ref([]);
+const searchMode = ref(false)
+/////////////////
+
+// computed 
+const searchResultEmpty = computed(()=> searchMode.value && searchData.value.length <= 0)
+/////////////////
+
+// methods
+const getSortedContactList = async () => {
+  sortedData.value = await getSortedData({target: 'contacts', sortvalue: 'fullname'});
+}
+
+const searchDataInDb = async (querry, selectedVal) => {
+    const empty = (querry === '' && selectedVal === '')
+    const selectEmpty = selectedVal === ''
+    searchMode.value = !empty
+    searchData.value  = await searchInDbByKeyWords({
+        target: 'contacts',
+        where: ['name', 'surname', 'patronymic', 'email', 'phonenumber'],
+        words: querry,
+        filterResult: !selectEmpty,
+        filtertarget: 'group',
+        filtervalue: selectedVal
+    })
+}
+
+// hooks
+onBeforeMount(()=> getSortedContactList())
+/////////////////
+
+</script>
+
+<style scoped>
+  .main-view-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .heading-logo,
+  .header-button {
+    display: flex;
+    align-items: center;
+  }
+
+  .main-view-body {
+    height: calc(100vh - 105.98px);
+  }
+
+  .body-search-bar {
+    margin-bottom: 15px;
+  }
+
+</style>
