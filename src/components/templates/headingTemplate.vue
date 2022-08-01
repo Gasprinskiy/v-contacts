64afd17f9ab36e8ad692aea65c7e4a0bfa10a21e
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-08-02 01:15:52 +0500

    header template

---
+++ src/components/templates/headingTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="heading">
+        <div class="heading-left">
+            <slot name="left-side"/>
+        </div>
+        <div class="header-right">
+            <slot name="right-side"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .heading {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 15px;
+    }
+</style>
