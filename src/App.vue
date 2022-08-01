a2c948e11ff1ea7d20e93fd50eebd610242d237a
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-08-02 01:16:56 +0500

    added notification provider

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<n-notification-provider placement="bottom-right" >
+  <div class="app-wrapper">
+    <div class="app-router-view">
+      <router-view/>
+    </div>
+  </div>
+</n-notification-provider>
+</template>
+
+<script setup>
+// imports
+import { NNotificationProvider } from 'naive-ui'
+///////////
+</script>
+
+<style scoped>
+  .app-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+</style>
