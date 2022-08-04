65ec1a99859179b0921bc82a5b0a789b25cdca99
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-08-04 16:30:01 +0500

    added nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
 <n-notification-provider placement="bottom-right" >
+  <div class="nav-bar">
+    <n-button @click="router.back()">
+      <template #icon>
+        <n-icon
+          :component="CaretBackSharp"
+        />
+      </template>
+    </n-button>
+  </div>
   <div class="app-wrapper">
     <div class="app-router-view">
       <router-view/>
@@ -9,9 +18,18 @@
 </template>
 
 <script setup>
+
 // imports
-import { NNotificationProvider } from 'naive-ui'
+import { NNotificationProvider, NButton, NIcon } from 'naive-ui'
+import { CaretBackSharp } from '@vicons/ionicons5'
+import { onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
 ///////////
+
+// data
+const router = useRouter()
+///////////
+
 </script>
 
 <style scoped>
@@ -21,4 +39,12 @@ import { NNotificationProvider } from 'naive-ui'
     flex-direction: column;
   }
 
+  .nav-bar {
+    width: 100%;
+    background-color: rgba(24, 160, 88, 0.15);
+    position: absolute;
+    border-radius: 3px;
+    left: 0;
+    top: 0;
+  }
 </style>
