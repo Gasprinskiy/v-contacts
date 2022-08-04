d28fbdbe17c1280461085cb63bf1702e1755c16b
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-08-04 16:29:08 +0500

    added creation verification

--- src/views/ContactCreateView.vue
+++ src/views/ContactCreateView.vue
@@ -3,10 +3,12 @@
 </template>
 
 <script setup>
+
 // imports
 import { useRouter } from 'vue-router';
-import { useNotification } from 'naive-ui'; 
-import { addData } from '../services/dbRequests/'
+import { useNotification,  NButton } from 'naive-ui'; 
+import { h } from 'vue'
+import { addData, getDataByKeyValue } from '../services/dbRequests/'
 import contactForm from '../components/contactForm.vue' 
 /////////////
 
@@ -17,7 +19,28 @@ const notif = useNotification()
 
 // methods
 const saveCreatedContantToDb = async (contact) => {
-    const result = await addData({target:'contacts', payload: contact})
+    const has = await getDataByKeyValue({target:  'contacts', keyValue: {phonenumber: contact.phonenumber} })
+    if(has) {
+        const n = notif.create({
+            type: 'warning',
+            title: 'Contact with the given phone number allready exist',
+            duration: 5000,
+            action: () => h(NButton, {
+                text: true,
+                type: "primary",
+                onClick: () => {
+                    router.push(`/contact-edit/${has.id}`)
+                    n.destroy();
+                }
+                }, 
+                {
+                    default: () => "View contact"
+                }
+            )
+        })
+        return
+    }
+    const result = await addData({target: 'contacts', payload: contact})
     await router.push(`/contact-edit/${result}`)
     notif.success({
         title: 'Contact crated',
